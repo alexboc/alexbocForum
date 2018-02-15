116f4c28c9dc29524371fdd34d6de63cf6b3bab8
HAIHANG LI <1772729359@qq.com>
2018-02-15 14:42:04 +0800

    edit settings.py

--- alexbocForum/alexbocForum/settings.py
+++ alexbocForum/alexbocForum/settings.py
@@ -118,3 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# works when deployment, collecting all static files in each app and
+# common static files in STATICFILES_DIRS to STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+
+# common static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'common_static')
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
