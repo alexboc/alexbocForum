3bc9dcdd18b9dfd8d7e619b2e8ac980f67f1f371
alexandrali <fjcyslyz@gmail.com>
2018-02-18 23:22:19 +0800

    Redirect to the index page after clicking 'sign in' button

--- alexbocForum/User/views.py
+++ alexbocForum/User/views.py
@@ -2,6 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def login(request):
-    return render(request, 'User/login.html', {})
+    if request.method == 'POST':
+        username = request.POST['user_name']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse('Forum:index'))
+        else:
+            return render(request, 'User/login.html')
+    else:
+        return render(request, 'User/login.html')
