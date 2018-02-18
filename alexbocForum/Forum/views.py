624645a80f2d5553b6e3c2f97767775527f5c8a9
alexandrali <fjcyslyz@gmail.com>
2018-02-18 23:37:11 +0800

    Add function: if user is not logged in, redirect to the login page

--- alexbocForum/Forum/views.py
+++ alexbocForum/Forum/views.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     user = request.user
-    return render(request, 'Forum/index.html', {'user': user})
+    if user.is_authenticated:
+        print('I am ' + user.username)
+        return render(request, 'Forum/index.html')
+    else:
+        return HttpResponseRedirect(reverse('User:login'))
