28f1911f2e61d0c642fe7d995d1e719f8e52f622
alexandrali <fjcyslyz@gmail.com>
2018-02-18 21:40:40 +0800

    add urls and views of index and login

--- alexbocForum/Forum/views.py
+++ alexbocForum/Forum/views.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    user = request.user
+    return render(request, 'Forum/index.html', {'user': user})
--- alexbocForum/alexbocForum/urls.py
+++ alexbocForum/alexbocForum/urls.py
@@ -13,15 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 import django
 
+from Forum.views import index
+
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^User/', include('User.urls')),
+    url(r'^Forum/', include('Forum.urls')),
 ]
 
 urlpatterns += url(r'^media/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.MEDIA_ROOT}),
