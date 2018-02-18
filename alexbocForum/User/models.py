ac1061f97a5646e06f0ac6df145c1372dc92f950
alexandrali <fjcyslyz@gmail.com>
2018-02-18 16:02:02 +0800

    modify model User -- match model User with AUTH_USER_MODEL

--- alexbocForum/User/models.py
+++ alexbocForum/User/models.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from alexbocForum import settings
+from alexbocForum import settings as mysettings
+from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 import hashlib
 import os
-
-def encry(password):
-    p = hashlib.md5()
-    p.update(password)
-    return p.hexdigest()
 
 def upload_avatar(instance, filename):
     fn, ext = os.path.splitext(filename)
     fn = instance.user_name
     filename = fn + ext
-    pre_file_dir = os.path.join(settings.MEDIA_ROOT, ('avatars/%s' % (filename))).replace('\\', '/')
+    pre_file_dir = os.path.join(mysettings.MEDIA_ROOT, ('avatars/%s' % (filename))).replace('\\', '/')
 
     if os.path.exists(pre_file_dir):
         os.remove(pre_file_dir)
@@ -24,17 +21,12 @@ def upload_avatar(instance, filename):
 
 @python_2_unicode_compatible
 class User(models.Model):
-    user_name = models.CharField('User Name', max_length=200, default='')
-    password = models.CharField('Password', max_length=200, default=encry('123456'), blank=True)
-    sign = models.CharField('Personal Signature', max_length=200, default='')
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='user',
+        verbose_name=_('User'), blank=True, null=True, default='')
+    nickname = models.CharField(_('Nickname'), max_length=255, blank=False, default='')
+    sign = models.CharField(_('Personal Signature'), max_length=255, default='')
     avatar = models.ImageField(upload_to=upload_avatar, default='avatars/default.jpg')
 
     def __str__(self):
-        return self.user_name
-
-    def __unicode__(self):
-        return self.user_name
-
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        return self.nickname or self.user.username
