2407aa51e81b9589e0ab4b1ec853fe05a5454ff7
alexandrali <fjcyslyz@gmail.com>
2018-02-19 21:24:41 +0800

    Change admin site of model UserProfile

--- alexbocForum/User/admin.py
+++ alexbocForum/User/admin.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import UserProfile
+from django.contrib.auth.admin import UserAdmin
 
 
-class UserProfileAdmin(admin.ModelAdmin):
+class UserProfileAdmin(UserAdmin):
     fieldsets = [
         ('User information', {'fields': ['username', 'password', 'nickname', 'sign',
     'avatar']}),
