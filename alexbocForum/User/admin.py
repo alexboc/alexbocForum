5d0317a7e8076406ec02c5e02f772c894d6554ec
alexandrali <fjcyslyz@gmail.com>
2018-02-19 18:41:16 +0800

    Modify model User

--- alexbocForum/User/admin.py
+++ alexbocForum/User/admin.py
@@ -4,4 +4,15 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User information', {'fields': ['username', 'password', 'nickname', 'sign',
+    'avatar']}),
+        ('Admin information', {'fields': ['is_staff', 'groups', 'user_permissions']}),
+    ]
+
+    list_display = ('username', 'avatar', 'is_staff')
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
